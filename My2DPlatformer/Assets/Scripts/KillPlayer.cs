2c3c32350c08c58cbb4b5038d8b55d90ec27bba4
RedWave <kurilov6dv@gmail.com>
2022-11-07 21:07:16 +0300

    CreateEnemy

---
+++ My2DPlatformer/Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log(collision.gameObject + " Умер");
+        }
+    }
+}
