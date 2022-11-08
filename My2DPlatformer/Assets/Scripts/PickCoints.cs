146291794553127270a2c5c30224e5a08ce0a84f
Lalamee <kurilov6dv@gmail.com>
2022-11-08 17:45:29 +0300

    AddCoins

---
+++ My2DPlatformer/Assets/Scripts/PickCoints.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PickCoints : MonoBehaviour
+{
+    private int _countCoints = 0;
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Coin")
+        {
+            Destroy(collision.gameObject);
+            _countCoints++;
+            Debug.Log(_countCoints + " монет у игрока");
+        }
+    }
+}
---
+++ My2DPlatformer/Assets/Scripts/SpawnCoins.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour
+{
+    [SerializeField] private Coin _goldCoin;
+    [SerializeField] private Transform _spawnPoints;
+
+    private Transform[] _points;
+    void Start()
+    {
+        _points = new Transform[_spawnPoints.childCount];
+
+        for (int i = 0; i < _spawnPoints.childCount; i++)
+        {
+            _points[i] = _spawnPoints.GetChild(i);
+        }
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            Instantiate(_goldCoin, _points[i].transform.position, Quaternion.identity);
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
